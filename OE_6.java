f78595b10026acfb8692825eb1391ddfa71e306a
MikeVillegas00 <90896686+MikeVillegas00@users.noreply.github.com>
2023-04-14 10:13:02 +0800

    Add files via upload

--- OE_6.java
+++ OE_6.java
@@ -1,54 +1,79 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class OE_6 {
 
-    public static int QSort(int[] arr, int low, int high) {
-            int pivot = arr[high];
-            int k = low - 1;
-            
-            for (int i = low; i <= high-1; i++) {
-                if (arr[i] <= pivot) {
-                    k++;
-                    int temp = arr[k];
-                    arr[k] = arr[i];
-                    arr[i] = temp;
-                }
+    //Void Functions
+    public static void Insertion(int[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int key = arr[i];
+            int k = i - 1;
+            while (k >= left && arr[k] > key) {
+                arr[k + 1] = arr[k];
+                k--;
             }
-            
-            int temp = arr[k+1];
-            arr[k+1] = arr[high];
-            arr[high] = temp;
-            
-            return k+1;
+            arr[k + 1] = key;
         }
+    } 
 
-    public static void Sort(int[] array, int low, int high) {
-        if (low < high) {
-            int Index = QSort(array, low, high);
-            Sort(array, low, Index-1);
-            Sort(array, Index+1, high);
+    public static int Partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[right];
+        arr[right] = temp;
+        return i + 1;
+    }
+
+    public static void Hybrid(int[] arr, int left, int right, int threshold) {
+        if (left >= right) {
+            return;
+        }
+        if (right - left + 1 <= threshold) {
+            Insertion(arr, left, right);
+        } else {
+            int pivotIndex = Partition(arr, left, right);
+            Hybrid(arr, left, pivotIndex - 1, threshold);
+            Hybrid(arr, pivotIndex + 1, right, threshold);
         }
     }
-      
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.print("\nEnter array size: ");
+        // Input array size and threshold
+        System.out.print("Enter array size: ");
         int n = sc.nextInt();
         
+        System.out.print("Threshold: ");
+        int threshold = sc.nextInt();
+        
+        //Random Generator
         int[] arr = new int[n];
-        System.out.print("\nEnter unsorted array elements: ");
+        Random random = new Random();
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+            arr[i] = random.nextInt(100);
         }
         
-        Sort(arr, 0, n-1);
+
+        //Unsorted Array
+        System.out.println("Unsorted array:");
+        System.out.println('[' + Arrays.toString(arr) + ']');
+
+        //Hybrid Array
+        Hybrid(arr, 0, n - 1, threshold);
         
-        System.out.println("Sorted array: ");
-        for (int i = 0; i < n; i++) {
-            System.out.println("[" + arr[i] + "]");
-        }
+        //Sorted Array
+        System.out.println("Sorted array:");
+        System.out.println('[' + Arrays.toString(arr) + ']');
     }
-    
-    
 }
+
+
